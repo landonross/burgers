89077d9e2a79048f5bda232aba11b56e474b03a5
Landon Ross <landon.w.ross@gmail.com>
2021-03-13 13:44:37 -0700

    template code taken and altered for this project

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Cheese');
+INSERT INTO burgers (name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (name, devour) VALUES ('Hawaiian', true);
+INSERT INTO burgers (name, devour) VALUES ('Cowboy', true);
+INSERT INTO burgers (name, devour) VALUES ('Heart Attack', true);
+INSERT INTO burgers (name) VALUES ('Soaked');
